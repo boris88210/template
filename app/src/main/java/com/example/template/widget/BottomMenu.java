e2091442fbf70f489a61a339d9099e0f95f8101e
Boris_Su <boris88210@gmail.com>
2020-07-04 16:38:49 +0800

    Initial project

---
+++ app/src/main/java/com/example/template/widget/BottomMenu.java
@@ -0,0 +1,22 @@
+package com.example.template.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class BottomMenu extends LinearLayout {
+
+
+    public BottomMenu(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+
+
+
+
+}
